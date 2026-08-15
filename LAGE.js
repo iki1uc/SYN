62380cecda3d2c9c4cf3165c0286e0df7e448299
iki1uc <entertrainer@web.de>
2026-08-15 08:58:29 +0200

    Update LAGE.js

--- LAGE.js
+++ LAGE.js
@@ -1,31 +1,5 @@
-export class Vector {
-    constructor(x, y, z){
-        this.x = x;
-        this.y = y;
-        this.z = z;
+export const LAGE = {
+    set(){
+        return { status: "neutral" };
     }
-
-    magnitude(){
-        return Math.sqrt(this.x**2 + this.y**2 + this.z**2);
-    }
-
-    curvature(k){
-        return {
-            cx: this.x * Math.cos(k),
-            cy: this.y * Math.sin(k),
-            cz: this.z * Math.tan(k)
-        };
-    }
-
-    fly(k){
-        const c = this.curvature(k);
-        return new Vector(c.cx, c.cy, c.cz);
-    }
-}
-export const SYN_State = {
-    CO: false,
-    TOR: false,
-    LAGE: false,
-    RECALL: false
 };
-
