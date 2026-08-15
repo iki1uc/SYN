6c676571d981e99d2ac47f7c4cad1512aa849cba
iki1uc <entertrainer@web.de>
2026-08-15 08:30:06 +0200

    Delete boom.parser.js

--- boom.parser.js
+++
@@ -1,9 +0,0 @@
-export async function BOOM_PARSE(path = "./boom.csv") {
-  const raw = await fetch(path).then(r => r.text());
-  const lines = raw.trim().split("\n").slice(1);
-
-  return lines.map(line => {
-    const [etage, modul, aufgabe] = line.split(",");
-    return { etage, modul, aufgabe };
-  });
-}
